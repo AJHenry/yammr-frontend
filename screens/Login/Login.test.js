8992d680540a21e679d5badbe11c2818ed9f8389
Andrew Henry <ajh145@pitt.edu>
2018-10-20 22:51:24 -0400

    Removed test for now

--- screens/Login/Login.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ThemePreview from './ThemePreview';
-
-import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<ThemePreview />).toJSON();
-  expect(rendered).toBeTruthy();
-});
