5ea7c46dedb22a3bd513beb65481799e8f4ca9a9
Andrew Henry <imandrewh@outlook.com>
2018-11-13 09:23:18 -0500

    Made bottom component it's own controller

--- components/BottomComment/BottomComment.js
+++ components/BottomComment/BottomComment.js
@@ -4,30 +4,66 @@ import { styles, style } from './BottomComment.styles';
 import { Icon } from 'react-native-elements';
 import { LargeInput } from '../index';
 
-export const BottomComment = props => {
-  const { cameraHandler, commentHandler, handleInputChange, value } = props;
-  return (
-    <View style={styles.comment}>
-      <Icon
-        iconStyle={style.extraButton}
-        type="simple-line-icon"
-        name="camera"
-        onPress={() => cameraHandler()}
-        component={TouchableHighlight}
-      />
-      <LargeInput
-        style={styles.commentInput}
-        placeholder="Say something.."
-        onChange={handleInputChange}
-        value={value}
-      />
-      <Icon
-        iconStyle={style.commentButton}
-        type="simple-line-icon"
-        name="paper-plane"
-        onPress={() => commentHandler()}
-        component={TouchableHighlight}
-      />
-    </View>
-  );
-};
+export class BottomComment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: null,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.commentHandler = this.commentHandler.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    console.log(`${name}, ${value}`);
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  /**
+   * Used for handling comment button
+   */
+  commentHandler() {
+    console.log(`Comment Handler, comment data: ${this.state.comment}`);
+  }
+
+  /**
+   * Used for handling camera button
+   */
+  cameraHandler = () => {
+    console.log(`Camera Handler`);
+  };
+
+  render() {
+    return (
+      <View style={styles.comment}>
+        <Icon
+          iconStyle={style.extraButton}
+          type="simple-line-icon"
+          name="camera"
+          onPress={() => this.cameraHandler()}
+          component={TouchableHighlight}
+        />
+        <LargeInput
+          style={styles.commentInput}
+          placeholder="Say something.."
+          onChangeText={text => this.setState({ comment: text })}
+        />
+        <Icon
+          iconStyle={style.commentButton}
+          type="simple-line-icon"
+          name="paper-plane"
+          onPress={() => this.commentHandler()}
+          component={TouchableHighlight}
+        />
+      </View>
+    );
+  }
+}
