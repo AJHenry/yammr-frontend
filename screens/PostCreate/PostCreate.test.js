be280f8c0708c25ad79ac59958260938e20f14f6
Andrew Henry <imandrewh@outlook.com>
2018-11-13 13:18:14 -0500

    Change test to pass in navigation prop

--- screens/PostCreate/PostCreate.test.js
+++ screens/PostCreate/PostCreate.test.js
@@ -4,6 +4,11 @@ import PostCreate from './PostCreate';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<PostCreate />).toJSON();
+  const navigation = {
+    goBack: jest.fn(),
+  };
+  const rendered = renderer
+    .create(<PostCreate navigation={navigation} />)
+    .toJSON();
   expect(rendered).toBeTruthy();
 });
